5447d3a2972dbfcf46bc0a134caef29a6c372c21
hunb8736 <131357133+hunb8736@users.noreply.github.com>
2023-05-03 01:20:06 +0300

    Create POW.sol

---
+++ POW.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.8.0;
+
+contract AssetOwnership {
+    struct Asset {
+        address owner;
+        string name;
+        // add more properties as needed
+    }
+
+    mapping (uint256 => Asset) private assets;
+
+    event AssetCreated(uint256 id, address owner);
+    event AssetTransferred(uint256 id, address oldOwner, address newOwner);
+
+    function createAsset(uint256 id, string memory name) public {
+        require(assets[id].owner == address(0), "Asset already exists");
+        assets[id] = Asset(msg.sender, name);
+        emit AssetCreated(id, msg.sender);
+    }
+
+    function getAssetOwner(uint256 id) public view returns (address) {
+        return assets[id].owner;
+    }
+}
